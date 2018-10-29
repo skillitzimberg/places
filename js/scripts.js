5bb39d3d4c9fb2df4e8fc24951a7de00a18e9a8e
ryan leslie and Scott Bergler <ryan@>
2018-10-29 13:57:30 -0700

    add variables and click functions to webpage

---
+++ js/scripts.js
@@ -0,0 +1,49 @@
+function Country(name) {
+  this.name = name,
+  this.cities = []
+}
+
+function City(name, longitude, latitude, landmarks) {
+  this.name = name,
+  this.latitude = latitude,
+  this.longitude = longitude,
+  this.landmarks = [],
+  this.notes = ""
+}
+
+Country.prototype.addCity = function(city) {
+  this.cities.push(city);
+}
+
+City.prototype.fullCity = function() {
+  return this.name + " " + this.longitude + " " +  this.latitude + " " +  this.landmarks;
+}
+
+var kenya = new Country("Kenya");
+var malaysia = new Country("Malaysia");
+var columbia = new Country("Columbia");
+
+var baragoi = new City("Baragoi", "1º47'N", "36º47'E");
+var kuta = new City("Kuta", "1º32", "1º45");
+var bogota = new City("Bogota","1º23", "1º50");
+
+kenya.addCity(baragoi);
+malaysia.addCity(kuta);
+columbia.addCity(bogota);
+
+
+
+// USER INTERFACE LOGIC
+$(document).ready(function() {
+  $("#kenya").click(function() {
+      $("#output").text(baragoi.fullCity());
+  });
+
+  $("#malaysia").click(function() {
+      $("#output").text(kuta.fullCity());
+  });
+
+  $("#columbia").click(function() {
+      $("#output").text(bogota.fullCity());
+  });
+});
